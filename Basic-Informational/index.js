e9b91dadb7ef9bb06a3388609434f6b25a5da763
Ethan Aurellano <ethanaurellano@Ethans-MacBook-Pro.local>
2022-09-16 12:17:07 -0700

    Push projects

---
+++ Basic-Informational/index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('Whoops! File not found');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    })
+};
+
+http.createServer(handleRequest).listen(8000);
