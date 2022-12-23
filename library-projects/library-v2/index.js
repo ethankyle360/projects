3932e8051f5f8b7d0bfdf6c3aa2cc479062e54da
ethankyle360 <kylemaurellano@gmail.com>
2022-12-23 11:04:44 -0700

    Added Landing Page

---
+++ library-projects/library-v2/index.js
@@ -0,0 +1,14 @@
+// create library list array
+// array of objects
+let libraryList = [
+    {
+        title: "Game of Thrones",
+        author: "George R. R. Martin",
+        pages: 694,
+        read: false
+    }
+];
+
+// dom objects
+$newButton = document.querySelector('.new');
+console.log($newButton);
